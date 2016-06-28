82fb7567f34a8b9bd6e3a81634f88bdac0d86f34
Jonathan Barnoud <jonathan@barnoud.net>
2016-06-28 18:08:59 +0200

    Add a framework for regression tests.
    Regression tests run insane with a set of arguments and compare the
    output with a reference. This allows to assess that the output did not
    change between the version of insane with which the reference has been
    generated and the current version of insane. It does not test that the
    result is valid!
    
    Run tests/test_regression.py as a script to generate the reference
    files. Run it with pytest or nosetest to execute the tests.
    
    As is, the framework does not work because the output of insane is
    randomize. This needs to be fixed.

---
+++ tests/test_regression.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+
+"""
+Regression tests for insane.
+
+This test suite runs the insane command line with various set of arguments, and
+assess that the results correspond to the result obtained with previous
+versions.
+
+Notice that these tests do not assess that the results are correct. Instead,
+they assess that changes do not affect the behaviour of the program.
+
+If ran as a script, this generate the reference files expected by the tests. If
+ran usinf pytest or nosetest, this executes insane with a series of arguments
+and compares the output to the reference.
+"""
+
+from __future__ import print_function
+
+import contextlib
+import difflib
+import os
+import shutil
+import shlex
+import subprocess
+import tempfile
+
+from StringIO import StringIO
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+INSANE = os.path.abspath(os.path.join(HERE, '../insane.py'))
+DATA_DIR = os.path.join(HERE, 'data')
+
+# The arguments to test insane with are listed here. The tuple is used both to
+# generate the references, and to run the tests.
+# To add a test case, add the arguments to test in the tuple.
+SIMPLE_TEST_CASES = (
+    '-o test.gro',
+    '-o test.gro -box 10,15,20',
+    '-o test.gro -box 10,15,20 -l POPC',
+)
+
+
+class ContextStringIO(StringIO):
+    """
+    StringIO but usable as a context manager.
+
+    StringIO can not completely pass as a file, because it is not usable as a
+    context manager in a 'with' statement. This class adds the context manager
+    hability to StringIO. It does nothing when the context manager is either
+    entered or exited, but it can be used in a 'with' statement.
+    """
+    def __enter__(self):
+        """
+        Does nothing when entering a 'with' statement.
+        """
+        pass
+
+    def __exit__(self, *args):
+        """
+        Does nothing when exiting a 'with' statement.
+        """
+        pass
+
+@contextlib.contextmanager
+def tempdir():
+    """
+    Context manager that moves in a temporary directory.
+
+    .. code::
+
+        # We are in the initial working directory
+        with tempdir():
+            # We are now in a temporary directory
+            ...
+        # We are back to the initial working directory.
+        # The temporary does not exist anymore.
+    """
+    return_dir = os.getcwd()
+    dirpath = tempfile.mkdtemp()
+    try:
+        os.chdir(dirpath)
+        yield dirpath
+    finally:
+        os.chdir(return_dir)
+        shutil.rmtree(dirpath)
+
+
+def _arguments_as_list(arguments):
+    """
+    Return the arguments as a list as expected by subprocess.Popen.
+
+    The arguments can be provided as a string that will be spitted to a list.
+    They can also be provided as a list, then the list will be returned
+    untouched.
+    """
+    try:
+        arguments_list = shlex.split(arguments)
+    except ValueError:
+        arguments_list = arguments
+    return arguments_list
+
+
+def _gro_output_from_arguments(arguments):
+    """
+    Find the file name of the GRO output provided as argument to insane.
+
+    The file name is passed to insane via the '-o' argument. If the argument is
+    provided several times, then only the last one is considered.
+
+    This function reads the arguments provided as a list of arguments.
+    """
+    for i, argument in reversed(list(enumerate(arguments))):
+        if argument == '-o':
+            o_index = i
+            break
+    else:
+        raise ValueError('Output GRO name is not provided to insane. '
+                         'Missing -o argument.')
+    return arguments[i + 1]
+
+
+def _open_if_needed(handle):
+    """
+    Return handle if it is a ContextStringIO instance else try to open it.
+    """
+    if isinstance(handle ContextStringIO):
+        return handle
+    return open(handle)
+
+
+def compare(output, reference):
+    """
+    Assert that two files are identical.
+    """
+    out_file = _open_if_needed(output)
+    ref_file = _open_if_needed(reference)
+    with out_file, ref_file:
+        out_content = out_file.readlines()
+        ref_content = ref_file.readlines()
+        diff = list(difflib.context_diff(out_content, ref_content))
+    if diff:
+        #for line in diff:
+        #    print(line, end='')
+        raise AssertionError('The two files are not identical.')
+
+
+def run_and_compare(arguments, ref_gro, ref_stdout, ref_stderr):
+    # Create the command as a list for subprocess.Popen.
+    # The arguments can be pass to the current function as a string or as a
+    # list of arguments. If they are passed as a string, they need to be
+    # converted to a list.
+    command = [INSANE] + _arguments_as_list(arguments)
+
+    # The name of the output gro file must be provided to insane for insane to
+    # work. Since we also need that file name, let's get it from insane's
+    # arguments.
+    gro_output = _gro_output_from_arguments(command)
+
+    # We want insane to run in a temporary directory. This allows to keep the
+    # file system clean, and it avoids mixing output of different tests.
+    with tempdir():
+        process = subprocess.Popen(command,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        out, err = process.communicate()
+        assert os.path.exists(gro_output)
+        compare(gro_output, ref_gro)
+        compare(ContextStringIO(out), ref_stdout)
+        compare(ContextStringIO(err), ref_stderr)
+
+
+def test_simple_cases():
+    simple_case_ref_data = os.path.join(DATA_DIR, 'simple_case')
+    for case in SIMPLE_TEST_CASES:
+        ref_gro = os.path.join(simple_case_ref_data, case + '.gro')
+        ref_stdout = os.path.join(simple_case_ref_data, case + '.out')
+        ref_stderr = os.path.join(simple_case_ref_data, case + '.err')
+        def _test_case():
+            run_and_compare(case, ref_gro, ref_stdout, ref_stderr)
+        yield _test_case
+
+
+def generate_simple_case_references():
+    simple_case_ref_data = os.path.join(DATA_DIR, 'simple_case')
+    for case in SIMPLE_TEST_CASES:
+        command = [INSANE] + _arguments_as_list(case)
+        out_gro = _gro_output_from_arguments(command)
+        ref_gro = os.path.join(simple_case_ref_data, case + '.gro')
+        ref_stdout = os.path.join(simple_case_ref_data, case + '.out')
+        ref_stderr = os.path.join(simple_case_ref_data, case + '.err')
+        with tempdir():
+            print(' '.join(command))
+            process = subprocess.Popen(command,
+                                       stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE)
+            out, err = process.communicate()
+            with open(ref_stdout, 'w') as outfile:
+                for line in out:
+                    print(line, file=outfile, end='')
+            with open(ref_stderr, 'w') as outfile:
+                for line in err:
+                    print(line, file=outfile, end='')
+            shutil.copy2(out_gro, ref_gro)
+
+
+if __name__ == '__main__':
+    generate_simple_case_references()
