b39b982ff0e9fa0d3c1cffd7e774fcb494b5ef78
Jonathan Barnoud <jonathan@barnoud.net>
2017-03-25 16:16:10 +0100

    Move version number is a separate file.

--- insane/__init__.py
+++ insane/__init__.py
@@ -16,4 +16,22 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+__authors__ = ["Tsjerk A. Wassenaar", "Jonathan Barnoud"]
+__year__    = "2017"
+
+
+# Read the version from a file to make sure 
+# that it is consistent with the one in setup.py.
+import os
+here = os.path.dirname(__file__)
+try:
+    with open(os.path.join(here, 'VERSION.txt')) as infile:
+        __version__ = infile.readline().strip()
+except:
+    __version__ = 'unknown'
+# Avoid poluting the namespace with useless stuff.
+del os
+del here
+
+
 from core import *
